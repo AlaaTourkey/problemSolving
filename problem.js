fc5d82617b89e9b79f2a82eecab03476de13ff3f
Alaa Tourkey <alaatourkey@gmail.com>
2025-09-08 02:39:09 +0300

    small edit

--- problem.js
+++ problem.js
@@ -1568,17 +1568,17 @@
 
 
 // ==========================================================================> Sum of differences in array
-// function sumOfDifferences(arr) {
-  //    arr.sort( (a , b)=> b - a )
+function sumOfDifferences(arr) {
+     arr.sort( (a , b)=> b - a )
   
-//    let sum = 0
-//    for (let i = 0; i < arr.length - 1; i++) {
-//      sum += arr[i] - arr[i + 1] ;
-//    }
-//    return sum;
+   let sum = 0
+   for (let i = 0; i < arr.length - 1; i++) {
+     sum += arr[i] - arr[i + 1] ;
+   }
+   return sum;
 
-// }
-// console.log(sumOfDifferences([1, 2, 10]));
+}
+console.log(sumOfDifferences([1, 2, 10]));
 
 // ==========================================================================> Find the smallest integer in the array
 function findSmallestInt(arr) {
